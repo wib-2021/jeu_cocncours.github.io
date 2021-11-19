1fa263b5a7b29b824ff167bce18306dea4f1d10e
wib-2021 <93648336+wib-2021@users.noreply.github.com>
2021-11-19 18:03:37 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,105 +1,73 @@
-let btnAdd = document.querySelector("#btn_add");
+
 let msgError = document.querySelector(".msg_error");
-let ul = document.querySelector(".list_names ul");
 let btnTrier = document.querySelector(".trier");
-let showWinLayout = document.querySelector(".show_winner") 
-let sortLayout = document.querySelector(".sort") 
+let showWinLayout = document.querySelector(".show_winner")
+let sortLayout = document.querySelector(".sort")
+let ul = document.querySelector("#lst1 > ul");
+
 
 let inputFile = document.querySelector("#inputFile");
 
-// inputFile.addEventListener('change', function () {
+inputFile.addEventListener('change', function () {
 
-//     const reader = new FileReader();
+    const reader = new FileReader();
 
-//     reader.onload = function () {
+    reader.onload = function () {
 
-//         let list = reader.result.split(" ");
+        let arrData = reader.result.split(" ");
 
+        for (let i = 0; i < arrData.length; i++) {
+            const element = arrData[i];
 
-//     }
+            let li = document.createElement("li");
+            let liTextNode = document.createTextNode(element);
 
-//     reader.readAsText(inputFile.files[0])
+            li.appendChild(liTextNode);
 
-//     if (inputFile.files.length > 0) {
-//         btnTrier.classList.remove("disabled")
-//     }
+            ul.appendChild(li)
 
-//     else
-//         btnTrier.classList.add("disabled")
-
-// })
-
-btnAdd.onclick = function (e) {
-
-    e.preventDefault();
-
-    let inputVal = document.querySelector("#text_val").value;
-
-    if (inputVal === "") {
-
-        showMessage("Merci de entrer les nom a trier ou bien ajouter une liste")
-
-    }
-
-    else {
-
-        let li = document.createElement("li");
-        let liText = document.createTextNode(inputVal);
-
-        li.appendChild(liText)
-
-        ul.appendChild(li);
-
-        if (ul.children.length > 1) {
-            btnTrier.classList.remove("disabled")
         }
-        else
-            btnTrier.classList.add("disabled")
+
     }
 
-}
+    reader.readAsText(inputFile.files[0])
+
+    if (inputFile.files.length > 0) {
+        btnTrier.classList.remove("disabled")
+    }
+
+    else
+        btnTrier.classList.add("disabled")
+
+})
+
 
 
 btnTrier.addEventListener("click", function (e) {
 
     e.preventDefault()
 
-
-    let ulChildren = Array.from(ul.children);
-    let winneIndex;
+    let arrLi = Array.from(ul.children);
 
 
-
-    sortLayout.style.display ="none";
-    showWinLayout.style.display ="block";
+    let winId = Math.floor(Math.random() * arrLi.length);
     
-    showWinLayout.querySelector("#lst2").appendChild(ul) ;
 
+    arrLi.forEach((ele)=>{
+        ele.classList.remove("winner")
+    })
 
-    // ulChildren = ulChildren.map((ele) => {
+    arrLi[winId].classList.add("winner");
 
-    //     return ele.innerHTML
-    // })
+    document.querySelector("#lst2").appendChild(ul);
 
-
-
-    // winneIndex = Math.floor(Math.random() * ulChildren.length);
+    showWinLayout.style.display = "block";
+    sortLayout.style.display="none"
 
 
 
 })
 
-
-
-function showMessage(str) {
-
-    msgError.innerHTML = str;
-    msgError.style.display = "block";
-
-    setTimeout(() => {
-        msgError.style.display = "none";
-    }, 2000);
-}
 
 
 
