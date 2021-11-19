e3ce8dcf5c4677c138820b1f8667a9100efa71d3
wib-2021 <93648336+wib-2021@users.noreply.github.com>
2021-11-19 16:19:31 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,22 +2,42 @@ let btnAdd = document.querySelector("#btn_add");
 let msgError = document.querySelector(".msg_error");
 let ul = document.querySelector(".elements_wraper ul");
 let btnTrier = document.querySelector(".trier");
+let showWinLayout = document.querySelector(".show_winner") 
+let sortLayout = document.querySelector(".sort") 
 
+let inputFile = document.querySelector("#inputFile");
+
+inputFile.addEventListener('change', function () {
+
+    const reader = new FileReader();
+
+    reader.onload = function () {
+
+        let list = reader.result.split(" ");
+
+
+    }
+
+    reader.readAsText(inputFile.files[0])
+
+    if (inputFile.files.length > 0) {
+        btnTrier.classList.remove("disabled")
+    }
+
+    else
+        btnTrier.classList.add("disabled")
+
+})
 
 btnAdd.onclick = function (e) {
 
-    let inputVal = document.querySelector("#text_val").value;
-
     e.preventDefault();
+
+    let inputVal = document.querySelector("#text_val").value;
 
     if (inputVal === "") {
 
-        msgError.innerHTML = " This is a danger alert—check it out!";
-        msgError.style.display = "block";
-
-        setTimeout(() => {
-            msgError.style.display = "none";
-        }, 1500);
+        showMessage("Merci de entrer les nom a trier ou bien ajouter une liste")
 
     }
 
@@ -30,22 +50,58 @@ btnAdd.onclick = function (e) {
 
         ul.appendChild(li);
 
-        checkIfMorThanTwo(ul)
+        if (ul.children.length > 1) {
+            btnTrier.classList.remove("disabled")
+        }
+        else
+            btnTrier.classList.add("disabled")
     }
 
 }
 
 
-function checkIfMorThanTwo(ul) {
+btnTrier.addEventListener("click", function (e) {
 
-    if (ul.children.length > 1) {
-        btnTrier.classList.remove("disabled")
-    }
-    else
-        btnTrier.classList.add("disabled")
+    e.preventDefault()
 
+
+    let ulChildren = Array.from(ul.children);
+    let winneIndex;
+
+  
+
+
+    sortLayout.style.display ="none";
+    showWinLayout.style.display ="block";
+    
+    showWinLayout.querySelector(".elements_wraper").innerHTML ;
+
+
+    ulChildren = ulChildren.map((ele) => {
+
+        return ele.innerHTML
+    })
+
+
+
+    winneIndex = Math.floor(Math.random() * ulChildren.length);
+
+
+
+})
+
+
+
+function showMessage(str) {
+    msgError.innerHTML = str;
+    msgError.style.display = "block";
+
+    setTimeout(() => {
+        msgError.style.display = "none";
+    }, 2000);
 }
 
-btnTrier.onclick = function (e) { 
-    e.preventDefault();
- }
+
+
+
+
