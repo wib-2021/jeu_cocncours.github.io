c94a14e81c09c6c37f03842749de727fe92266ef
wib-2021 <93648336+wib-2021@users.noreply.github.com>
2021-11-22 12:22:01 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 let msgError = document.querySelector(".msg_error");
 let btnTrier = document.querySelector(".trier");
-let showWinLayout = document.querySelector(".show_winner")
-let sortLayout = document.querySelector(".sort")
-let ul = document.querySelector("#lst1 > ul");
+let showWinLayout = document.querySelector(".lyWinner")
+let sortLayout = document.querySelector(".ly_upload");
+let container = document.querySelector(".container");
+let number = Math.ceil(Math.random() * 1000);
+
+
+
 
 
 let inputFile = document.querySelector("#inputFile");
@@ -15,49 +19,36 @@ inputFile.addEventListener('change', function () {
 
         let arrData = reader.result.split(" ");
 
-        for (let i = 0; i < arrData.length; i++) {
-            const element = arrData[i];
-
-            let li = document.createElement("li");
-            let liTextNode = document.createTextNode(element);
-
-            li.appendChild(liTextNode);
-
-            ul.appendChild(li)
-
-        }
     }
 
     reader.readAsText(inputFile.files[0]);
 
-    if (inputFile.files.length > 0) {
+    // if (inputFile.files.length > 0) {
 
-        btnTrier.classList.remove("disabled")
+    //     btnTrier.classList.remove("disabled")
 
-    }
+    // }
 
-    else
-        btnTrier.classList.add("disabled")
+    // else
+    //     btnTrier.classList.add("disabled")
 
 })
 
 
 btnTrier.addEventListener("click", function (e) {
 
+
     e.preventDefault();
 
-    let arrLi = Array.from(ul.children);
-
-    let winId = Math.floor(Math.random() * arrLi.length);
-
-    let lst2 = document.querySelector("#lst2")
 
 
-    lst2.appendChild(ul);
+    container.style.transform = `rotate(${number}deg)`;
+
+    number += Math.ceil(Math.random() * 1000)
 
 
-    showWinLayout.style.display = "block";
-    sortLayout.style.display = "none";
+    // showWinLayout.style.display = "block";
+    // sortLayout.style.display = "none";
 
 })
 
